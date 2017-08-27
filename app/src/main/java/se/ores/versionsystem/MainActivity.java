8e28d9970522441b5c73643e3142edfa6a01b507
jmalmer <jmalmer@telia.com>
2017-08-27 17:44:49 +0200

    second commit

--- app/src/main/java/se/ores/versionsystem/MainActivity.java
+++ app/src/main/java/se/ores/versionsystem/MainActivity.java
@@ -1,7 +1,7 @@
 package se.ores.versionsystem;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void Settings() {
+
+    }
 }
+
